deeaafea9c4634cc02e895f5ab0b6e3ba7807827
Patricio Villanueva <patricio0301@gmail.com>
2020-09-21 17:10:39 -0300

    arreglo register ejs

--- middlewares/registerValidation.js
+++ middlewares/registerValidation.js
@@ -19,32 +19,6 @@ module.exports = [
                     }
                 })
         }),
-    // body('email')
-    //     .custom((email, {req}) => {
-    //         db.Usuario.findOne({
-    //             where: {
-    //             email: req.body.email,
-    //         }})
-    //         .then(resultados=> {
-    //             console.log(resultados);
-    //             if (resultados != null){
-    //                 console.log("existe");
-    //                 return false;
-    //             }
-    //             // else{
-    //             //     console.log("existe");
-    //             //     return false;
-    //             // }
-    //         })
-    //         // let userList = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/users.json')), 'utf8');
-    //         // for(let i = 0; i < userList.length; i++) {
-    //         //         if(userList[i].email == valorDelCampo) {
-    //         //             return false;
-    //         //         }
-    //         //     }
-    //         // return true
-    //     })
-    //     .withMessage('El email utilizado ya pertecene a un usuario registrado!!!!'),
     check('password')
         .isLength({min: 8 , max: 12})
         .withMessage('La contraseña debe tener desde 8 y hasta 12 caracteres')
